488e85321b7418005606c5b64e44c6dc832fe702
MahmoudKhalidShabaan <m.k.sallam100200@gmail.com>
2024-11-17 16:19:39 +0300

    problem_14.py

--- problem_14.py
+++ problem_14.py
@@ -21,7 +21,7 @@ def get_all_arthimetic_operations_on_two_numbers(fri_num , sec_num) :
     print("the result of the addition arthimetic operation of the two numbers is = "+str(fri_num + sec_num))
     print("the result of the subtraction arthimetic operation of the two numbers is = "+str(fri_num - sec_num))
     print("the result of the multiplication arthimetic operation of the two numbers is = "+str(fri_num * sec_num))
-    print("the result of the division arthimetic operation of the two numbers is = "+str(fri_num - sec_num))
+    print("the result of the division arthimetic operation of the two numbers is = "+str(fri_num / sec_num))
     print("the result of the modulus arthimetic operation of the two numbers is = "+str(fri_num % sec_num))
 frist_num = float(input("please enter the frist number is = "))
 second_num = float(input("please enter the second number is = "))
